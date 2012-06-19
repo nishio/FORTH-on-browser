be8c80709140121162c41d93b2c682877cc9865f
Pawel Marczewski <humpolec@gmail.com>
2012-06-19 22:26:15 +0200

    tests for stack manip

--- forth-test.js
+++ forth-test.js
@@ -26,6 +26,12 @@ forth.test = function() {
 
         forth.assertExec('5 3 0 1 + / -', '[2]');
 
+        forth.assertExec('2 3 swap dup', '[3, 2, 2]');
+        forth.assertExec('2 3 drop drop', '[]');
+        forth.assertExec('1 2 3 rot', '[2, 3, 1]');
+        forth.assertExec('1 2 3 -rot', '[3, 1, 2]');
+        forth.assertExec('1 2 over', '[1, 2, 1]');
+
         forth.stack.reset();
         forth.terminal.echo('All tests OK!');
     } catch (err) {
