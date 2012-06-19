3dd9cf6b780b62bec6c9cd501393be30caa3980e
Pawel Marczewski <humpolec@gmail.com>
2012-06-19 21:23:51 +0200

    test for forth.parse()

--- forth-test.js
+++ forth-test.js
@@ -11,6 +11,8 @@ function assertEqual(e1, e2) {
 forth.test = function() {
     forth.terminal.echo('Running tests...');
     try {
+        assertEqual(forth.printWords(forth.parse(' A b c  d ')),
+                          'a b c d');
         forth.terminal.echo('All tests OK!');
     } catch (err) {
         forth.terminal.error('test: '+err);
