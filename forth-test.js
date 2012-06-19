c814c6b2fab532ddba6e2160783868f499b4cbdb
Pawel Marczewski <humpolec@gmail.com>
2012-06-19 22:24:17 +0200

    assert functions inside 'forth' object

--- forth-test.js
+++ forth-test.js
@@ -1,30 +1,30 @@
 
 // Testing
 
-function assertEqual(e1, e2) {
+forth.assertEqual = function(e1, e2) {
     if (e1 != e2) {
         var err = 'Assertion failed: ' + e1 + ' != ' + e2;
         throw err;
     }
 };
 
-function assertExec(code, result) {
+forth.assertExec = function(code, result) {
     forth.execute(forth.parse(code));
-    assertEqual(forth.stack.print(), result);
+    forth.assertEqual(forth.stack.print(), result);
     forth.stack.reset();
 }
 
 forth.test = function() {
     forth.terminal.echo('Running tests...');
     try {
-        assertEqual(forth.printWords(forth.parse(' A b c  d ')),
+        forth.assertEqual(forth.printWords(forth.parse(' A b c  d ')),
                           'a b c d');
 
-        assertExec('2 3', '[2, 3]');
+        forth.assertExec('2 3', '[2, 3]');
 
-        assertExec('2 3 +', '[5]');
+        forth.assertExec('2 3 +', '[5]');
 
-        assertExec('5 3 0 1 + / -', '[2]');
+        forth.assertExec('5 3 0 1 + / -', '[2]');
 
         forth.stack.reset();
         forth.terminal.echo('All tests OK!');
