feece21815d7d0e8a4e3f6399686d6bff6a0f1d8
Pawel Marczewski <humpolec@gmail.com>
2012-06-19 21:52:01 +0200

    stack visualization

--- forth-interface.js
+++ forth-interface.js
@@ -1,13 +1,14 @@
 
 // Forth interface
 
-// Install Forth interface. terminal is a jQuery object
+// Install Forth interface. terminal, stack are jQuery objects
 forth.interface = function(terminal) {
     terminal.terminal(
-        function(str, terminal) {
+        function(str, terminal, stack) {
             try {
                 var words = forth.parse(str);
                 forth.execute(words);
+                forth.redrawStack();
             } catch(err) {
                 terminal.error(err);
             }
@@ -17,4 +18,14 @@ forth.interface = function(terminal) {
             prompt: '> '
         });
     forth.terminal = terminal.terminal();
+    forth.stackElt = $(stack);
+};
+
+forth.redrawStack = function() {
+    forth.stackElt.empty();
+    for (var i = 0; i < forth.stack.data.length; i++) {
+        var val = forth.stack.data[i];
+        var elt = $('<div class="stackElt">'+val+'</div>');
+        forth.stackElt.append(elt);
+    }
 };
